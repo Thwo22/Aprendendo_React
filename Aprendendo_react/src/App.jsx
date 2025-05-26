2e6b8d1aa9dc63e7d20de300cd375c29c0d5dff4
Thiago Santos <137359517+Thwo22@users.noreply.github.com>
2025-05-26 18:24:25 -0300

    1.2

--- Aprendendo_react/src/App.jsx
+++ Aprendendo_react/src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
+  const name = "Thiago";
 
   return (
     <>
       <div className="container">
-        <h1>Olá, Mundo!</h1>
+        <h1>Olá, {name}, este é  seu Mundo!</h1>
+        <h2>alterando jsx</h2>
         <div><p className='paragrafo'>testando como funciona o componente basico do react</p></div>
       </div>
     </>
