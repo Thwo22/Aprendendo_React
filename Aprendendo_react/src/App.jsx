86eddc4b73828cdd00976981e38b6fbaa77c97f5
Thiago Santos <137359517+Thwo22@users.noreply.github.com>
2025-05-29 14:13:39 -0300

    1.6

--- Aprendendo_react/src/App.jsx
+++ Aprendendo_react/src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import Frase from './components/frase';
 import SayMyName from "./components/SayMyName.1";
 import Pessoa from './components/pessoa';
+import List from './components/list';
 
 function App() {
 
@@ -10,8 +11,15 @@ function App() {
     <>
       <div className='container'>
         <Frase/>
-        <Pessoa nome="Thiago" idade="18 anos" profissao="Desenvolvedor Web" foto="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-GgSPYmslH8mRKnXcBJkcg4F5KGE1zQxKnA&s"/>
+        <Pessoa 
+          nome="Thiago" 
+          idade="18 anos"
+          profissao="Desenvolvedor Web"
+          foto="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-GgSPYmslH8mRKnXcBJkcg4F5KGE1zQxKnA&s"
+        />
+        <List/>
       </div>
+      
     </>
   )
 }
---
+++ Aprendendo_react/src/components/list.jsx
@@ -0,0 +1,14 @@
+function List () {
+    return (
+        <>
+            <h1>
+                <ul>
+                    <li>Item 1</li>
+                    <li>Item 2</li>
+                </ul>
+            </h1>
+        </>
+    )
+}
+
+export default List
